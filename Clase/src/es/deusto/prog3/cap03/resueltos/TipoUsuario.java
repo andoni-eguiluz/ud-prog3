80da83b322e0e0fc24cb9c1eb44d0b125337e765
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2022-09-13 21:23:59 +0200

    Inicio de clase 2022

--- Clase/src/es/deusto/prog3/cap03/resueltos/TipoUsuario.java
+++
@@ -1,9 +0,0 @@
-package es.deusto.prog3.cap03.resueltos;
-
-/** Tipo de los usuarios
- * @author Andoni Eguíluz Morán
- * Facultad de Ingeniería - Universidad de Deusto
- */
-public enum TipoUsuario {
-	Admin, Empleado, Cliente, Invitado
-}

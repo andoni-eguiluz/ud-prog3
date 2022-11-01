1e79e6af2e08c0dd01150fa7f87f35e7d915d6e8
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2022-11-01 17:27:09 +0100

    Ejemplo JTable para renderers y editors

---
+++ Clase/src/es/deusto/prog3/cap06/resueltos/EjemploRenderersYEditors.java
@@ -0,0 +1,64 @@
+package es.deusto.prog3.cap06.resueltos;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class EjemploRenderersYEditors {
+
+	public static void main(String[] args) {
+		JFrame ventana = new JFrame( "Ejemplo de comprensión de renderers/editors" );
+		ventana.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		ventana.setSize( 800, 600 );
+		JList<Usuario> listaUsuarios = new JList<>( 
+			new Usuario[] {
+				new Usuario( 1, "Olatz" ),
+				new Usuario( 2, "Asier" ),
+				new Usuario( 3, "Arantza" ),
+				new Usuario( 4, "Pablo" ),
+				new Usuario( 5, "Marian" )
+			}
+		);
+		JComboBox<String> cb = new JComboBox<>( new String[] { "Admins", "Gestores", "Usuarios" } );
+		ventana.add( cb, BorderLayout.NORTH );
+		ventana.add( new JScrollPane( listaUsuarios ), BorderLayout.WEST );
+		Object[] columns = { "Val 1", "Val 2", "Val 3", "Val 4" };
+		Object[][] datos = new Object[13][4];
+		for (int i=0; i<13; i++) {
+			for (int j=0; j<4; j++) {
+				datos[i][j] = (int) (Math.random()*1000);
+			}
+		}
+		JTable tabla = new JTable( datos, columns );
+		ventana.add( new JScrollPane( tabla ), BorderLayout.CENTER );
+		
+		ventana.setVisible( true );
+	}
+
+}
+
+// Clase de ejemplo
+class Usuario {
+	private int id;
+	private String nick;
+	public Usuario(int id, String nick) {
+		super();
+		this.id = id;
+		this.nick = nick;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	@Override
+	public String toString() {
+		return nick + " (" + id + ")";
+	}
+}

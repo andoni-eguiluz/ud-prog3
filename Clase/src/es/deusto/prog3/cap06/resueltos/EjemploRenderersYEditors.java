4fa8731ea00dd3d456482f84d4e4abe194e63de6
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2022-11-09 21:40:31 +0100

    Modelo de datos en JTable

--- Clase/src/es/deusto/prog3/cap06/resueltos/EjemploRenderersYEditors.java
+++ Clase/src/es/deusto/prog3/cap06/resueltos/EjemploRenderersYEditors.java
@@ -1,9 +1,17 @@
 package es.deusto.prog3.cap06.resueltos;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.EventObject;
+
 import javax.swing.*;
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
 
 /** Clase de ejemplo para entender el concepto de renderers y editors
  * @author andoni.eguiluz @ ingenieria.deusto.es
@@ -15,6 +23,7 @@ public class EjemploRenderersYEditors {
 		JFrame ventana = new JFrame( "Ejemplo de comprensión de renderers/editors" );
 		ventana.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		ventana.setSize( 800, 600 );
+		// ventana.setLocation( 2000, 0 );
 		JComboBox<String> cb = new JComboBox<>( new String[] { "Admins", "Gestores", "Usuarios" } );
 		ventana.add( cb, BorderLayout.NORTH );
 		JList<Usuario> listaUsuarios = new JList<>( 
@@ -79,6 +88,7 @@ public class EjemploRenderersYEditors {
 				// System.out.println( ret.getClass().getSuperclass().getSuperclass() );
 				JLabel l = (JLabel) ret;
 				l.setIcon( null ); // (**) Esto para evitar que se dupliquen los icons de la línea de abajo
+				l.setBackground( Color.WHITE );  // Lo mismo
 				// El cambio de tamaño del componente renderer no afecta a la celda (el tamaño de la celda lo determina la tabla, no el renderer que dibuja en el espacio que tiene)
 				// l.setMinimumSize( new Dimension( 200, 150 ) );  // No afecta
 				// l.setPreferredSize( new Dimension( 200, 150 ) );  // No afecta
@@ -89,7 +99,7 @@ public class EjemploRenderersYEditors {
 					l.setForeground( Color.BLUE );
 				}
 				else {
-					l.setForeground( Color.BLACK );
+				//	l.setForeground( Color.BLACK );
 				}
 				if (value instanceof Integer) {
 					int valor = (Integer) value;
@@ -103,6 +113,7 @@ public class EjemploRenderersYEditors {
 						// Para dibujar distinto hay que cambiar el dibujado del componente: no nos vale el JLabel normal
 						JLabel lCambiada = new JLabel( value.toString() ) {
 							protected void paintComponent(Graphics g) {
+								// System.out.println( "paintComponent " + value.toString() );
 								if (isSelected) {
 									g.setColor( Color.LIGHT_GRAY );
 								} else {
@@ -111,6 +122,7 @@ public class EjemploRenderersYEditors {
 								g.fillRect( 0,  0,  getWidth(),  getHeight() );
 								ImageIcon img = new ImageIcon( EjemploRenderersYEditors.class.getResource( "world.png" ) );
 								g.drawImage( img.getImage(), 0, 0, getWidth(), getHeight(), null );
+								g.drawLine( 0, 0, getWidth(), getHeight());
 								super.paintComponent( g );
 							}
 						};
@@ -130,6 +142,155 @@ public class EjemploRenderersYEditors {
 			}
 		} );
 
+		// EDITORES
+		tabla.setDefaultEditor( Object.class, new DefaultCellEditor( new JTextField() ) {
+			// TODO - Cambiar solo lo que quiera que se haga distinto
+		});
+		
+		tabla.setDefaultEditor( Object.class, new TableCellEditor() {
+			private ArrayList<CellEditorListener> lCEL = new ArrayList<>();
+			@Override
+			public boolean isCellEditable(EventObject anEvent) {
+				return true;
+			}
+			@Override
+			public void addCellEditorListener(CellEditorListener l) {
+				lCEL.add( l );
+				// System.out.println( "addCellEditorListener" );
+			}
+			@Override
+			public void removeCellEditorListener(CellEditorListener l) {
+				lCEL.remove( l );
+				// System.out.println( "removeCellEditorListener" );
+			}
+			@Override
+			public boolean stopCellEditing() {
+				for (int i = lCEL.size()-1; i>=0; i--) {
+					lCEL.get(i).editingStopped( new ChangeEvent(this) );
+				}
+				return true;
+			}
+			@Override
+			public void cancelCellEditing() {
+				for (int i = lCEL.size()-1; i>=0; i--) {
+					lCEL.get(i).editingCanceled( new ChangeEvent(this) );
+				}
+			}
+			private JTextField tf;
+			@Override
+			public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+				tf = new JTextField( value.toString() );
+				tf.setFont( new Font( "Arial", Font.PLAIN, 48 ) );
+				return tf;
+			}
+			@Override
+			public Object getCellEditorValue() {
+				try {
+					int valor = Integer.parseInt( tf.getText() );
+					return valor;
+				} catch (NumberFormatException e) {
+					// Si no es int, probamos si es double
+					try {
+						double valor = Double.parseDouble( tf.getText() );
+						return valor;
+					} catch (NumberFormatException e2) {
+						// Si no es double, entonces lo dejamos como string
+						return tf.getText();
+					}
+				}
+			}
+			@Override
+			public boolean shouldSelectCell(EventObject anEvent) {
+				return true;
+			}
+		});
+		
+		// MODELOS
+		Object[][] datosNuevos = new Object[][] {
+			{ 900, 200.5, "Itziar", false },
+			{ 400, 20.0, "Ignacio", true },
+			{ 300, 50.5, "Aitor", false },
+			{ 850, 100.0, "Luis", false }
+		};
+		String[] columnasNuevas = new String[] { "Id", "Saldo", "Nick", "Premium" };
+		TableModel modelo = new TableModel() {
+			// Los listeners
+			ArrayList<TableModelListener> lista = new ArrayList<>();
+			@Override
+			public void removeTableModelListener(TableModelListener l) {
+				lista.remove( l );
+			}
+			@Override
+			public void addTableModelListener(TableModelListener l) {
+				lista.add( l );
+			}
+
+			// Sin estas no hay nada que hacer (ver que sin tocarlas no se ve la tabla o casca)
+			@Override
+			public int getRowCount() {
+				return datosNuevos.length;
+			}
+			@Override
+			public int getColumnCount() {
+				return datosNuevos[0].length;
+			}
+			@Override
+			public Class<?> getColumnClass(int columnIndex) {
+				// Podríamos devolver un tipo distinto por cada columna, por ejemplo
+				// (esto nos podría servir para configurar renderers distintos por columna)
+				// El de String coge el de Object si no hay particular de String, sin embargo el de Boolean coge un checkbox por defecto
+				if (columnIndex==2) {
+					return String.class;
+				} else if (columnIndex==3) {
+					return Boolean.class;
+				}
+				return Object.class;
+			}
+
+			// Nada de esto sirve si el modelo no sabe QUÉ datos devolver
+			@Override
+			public Object getValueAt(int rowIndex, int columnIndex) {
+				// System.out.println( "getValue " + rowIndex + "," + columnIndex );
+				return datosNuevos[rowIndex][columnIndex];
+			}
+			
+			@Override
+			public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+				// System.out.println( "setValue " + rowIndex + "," + columnIndex + " = " + aValue );
+				datosNuevos[rowIndex][columnIndex] = aValue;
+				// ¿Cómo hacer que un entero siga siendo un entero?
+				// Lo podemos hacer en el propio editor, o forzarlo aquí en el modelo. Por ejemplo
+				// if (columnIndex==0) {
+				// 	try {
+				// 		int valor = Integer.parseInt( (String) aValue );
+				// 		datosNuevos[rowIndex][columnIndex] = Integer.valueOf( valor );
+				// 	} catch (NumberFormatException e) {}
+				// } else if (columnIndex==1) {
+				// 	try {
+				// 		double valor = Double.parseDouble( (String) aValue );
+				// 		datosNuevos[rowIndex][columnIndex] = Double.valueOf( valor );
+				// 	} catch (NumberFormatException e) {}
+				// }
+			}
+			
+			// Y esto no funciona si la celda no es editable (para poder editar tienen que ser true los dos isCellEditable, el editor y el modelo de datos)
+			@Override
+			public boolean isCellEditable(int rowIndex, int columnIndex) {
+				if (columnIndex==2) {
+					return false;
+				}
+				return true;
+			}
+			
+			// Y habíamos perdido las cabeceras!  Van también con el modelo: aquí están.
+			@Override
+			public String getColumnName(int columnIndex) {
+				return (new String[] { "Dato 1", "Dato 2", "Nombre", "¿Director/a?" })[columnIndex];
+			}
+		};
+		tabla.setModel( modelo );
+
+		
 		
 		ventana.setVisible( true );
 	}

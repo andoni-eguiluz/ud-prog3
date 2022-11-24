ddb3385dbcced70759b9ff60ff97335ed0bfd1f4
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2022-11-24 11:04:21 +0100

    Ejemplos recursivos

--- Clase/src/es/deusto/prog3/cap04/PruebasDeRecursividad.java
+++ Clase/src/es/deusto/prog3/cap04/PruebasDeRecursividad.java
@@ -2,14 +2,51 @@ package es.deusto.prog3.cap04;
 
 public class PruebasDeRecursividad {
 	
-	private static int numLlamadas;
+	private static long numLlamadas;
 	
 	public static void main(String[] args) {
 		numLlamadas = 0;
-		int varPrueba = 7;
+		// int varPrueba = 7;
 		// f( varPrueba );
-		conteo( 1 );
+		// conteo( 1 );
+		// System.out.println( factorial( 16 ) );
+		System.out.println( fib(45) );
+		System.out.println( numLlamadas );
 	}
+	
+	// 1,1,2,3,5,8,13,21...
+	// si n > 1, fib (n) = fib(n-1) + fib(n-2)
+	// si n = 1, fib(1) = 1
+	public static long fib( int n ) {
+		numLlamadas++;
+		if (n==1) {
+			return 1;
+		} else if (n==2) {
+			return 1;
+		} else {
+			return fib(n-1) + fib(n-2);
+		}
+	}
+	
+	
+	
+	// n! = 1 * 2 * 3 * 4 * ... * (n-2) * (n-1) * n
+	// si n > 1 --> n! = n * (n-1)!     [factorial(n) returns n * factorial(n-1)]
+	//    n == 0 --> n! = 1
+	/** Calcula el factorial de un número
+	 * @param n	Número del que calcular el factorial, debe ser mayor o igual a cero
+	 * @return	Factorial matemático de n
+	 */
+	public static long factorial( int n ) {
+		if (n==0) {  // Caso base
+			return 1;
+		} else {  // Caso recursivo
+			return n * factorial( n-1 );
+		}
+	} 
+
+	
+	
 	
 	// Contar (sacar a consola) de i a 4000:
 	//    - Caso recursivo:
@@ -18,15 +55,17 @@ public class PruebasDeRecursividad {
 	//    - Caso base: si i==4000
 	//      - sacar a consola 4000
 	private static void conteo( int i ) {
-		if (i==4000) {
-			System.out.println( 4000 );
+		if (i==5) {
+			System.out.println( i );
 		} else {
+			System.out.println( i );
 			conteo( i+1 );
 			System.out.println( i );
 		}
 	}
 	
-	private static void f( int param ) {
+	@SuppressWarnings("unused")
+	private static void f( long param ) {
 		param = param + numLlamadas;
 		numLlamadas++;
 		System.out.println( "soy f en la llamada " + numLlamadas + " cálculo " + param );

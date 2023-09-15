98ff851ba1af5a5371d5ebc3035803f3dafad44d
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2023-09-15 10:11:13 +0200

    Prueba de git

--- Clase/src/es/deusto/prog3/cap00/ejercicios/VentanaConfirmacionLenta.java
+++ Clase/src/es/deusto/prog3/cap00/ejercicios/VentanaConfirmacionLenta.java
@@ -25,6 +25,7 @@ public class VentanaConfirmacionLenta {
 	public static void main(String[] args) {
 		// TODO Desarrollar la clase de acuerdo a los comentarios de la cabecera
 		// Comentario de prueba
+		// Comentario 2
 	}
 
 }

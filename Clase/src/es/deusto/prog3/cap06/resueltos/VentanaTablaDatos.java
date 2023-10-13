773b68ffcab539b0a913bc5010d8bd61f52319b4
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2023-10-13 08:16:49 +0200

    Ejercicio 6.3 resuelto

--- Clase/src/es/deusto/prog3/cap06/resueltos/VentanaTablaDatos.java
+++ Clase/src/es/deusto/prog3/cap06/resueltos/VentanaTablaDatos.java
@@ -15,6 +15,9 @@ public class VentanaTablaDatos extends JFrame {
 	private JTable tablaDatos;
 	private DataSetMunicipios datosMunis;
 	private MiTableModel modeloDatos;
+
+	// Paso 11 - atributos
+	String autonomiaSeleccionada = "";
 	
 	public VentanaTablaDatos( JFrame ventOrigen ) {
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
@@ -54,6 +57,19 @@ public class VentanaTablaDatos extends JFrame {
 				}
 			}
 		});
+		
+		// Paso 6 - inserción
+		bAnyadir.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int filaSel = tablaDatos.getSelectedRow();
+				if (filaSel>=0) {
+					datosMunis.anyadir( new Municipio( datosMunis.getListaMunicipios().size()+1, "Nombre", 0, "Provincia", "Autonomía" ), filaSel );
+					modeloDatos.anyadeFila( filaSel );
+				}
+			}
+		});
+		
 	}
 	
 	public void setDatos( DataSetMunicipios datosMunis ) {
@@ -69,34 +85,119 @@ public class VentanaTablaDatos extends JFrame {
 		col.setMaxWidth( 150 );
 
 		// Paso 7
-		tablaDatos.setDefaultRenderer( Object.class, new DefaultTableCellRenderer() {
-			private JProgressBar pbHabs = new JProgressBar( 0, 5000000 );
+		tablaDatos.setDefaultRenderer( Integer.class, new DefaultTableCellRenderer() {
+			// private JProgressBar pbHabs = new JProgressBar( 0, 5000000 );
+			// Paso 10:
+			private JProgressBar pbHabs = new JProgressBar( 0, 5000000 ) {
+				protected void paintComponent(java.awt.Graphics g) {
+					super.paintComponent(g);
+					g.setColor( Color.BLACK );
+					g.drawString( getValue()+"", 50, 10 );
+				}
+			};
 			@Override
 			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
 					boolean hasFocus, int row, int column) {
 				System.out.println( "getTCR " + row + "," + column );
 				if (column==2) {
-					int valorCelda = Integer.parseInt( value.toString() );
-					pbHabs.setValue( valorCelda );
+					// Si el dato es un Object o String sería esto
+					// int valorCelda = Integer.parseInt( value.toString() );
+					// pbHabs.setValue( valorCelda );
+					// return pbHabs;
+					// Pero si el dato está asegurado ser un Integer se puede castear:
+					pbHabs.setValue( (Integer)value );
 					return pbHabs;
 				}
 				JLabel rendPorDefecto = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-				if (row % 2 == 0) {
-					rendPorDefecto.setBackground( Color.CYAN );
-				}
 				return rendPorDefecto;
 			}
 			
+		});
+		
+		// Paso 11
+		tablaDatos.setDefaultRenderer( String.class, new DefaultTableCellRenderer() {
+			@Override
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+				Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column );
+				c.setBackground( Color.WHITE );
+				if (isSelected) {  // Observa que si no se pone esto uno no se entera cuándo la fila está seleccionada
+					c.setBackground( Color.LIGHT_GRAY );
+				}
+				if (column==4) { // Paso 11 del ejercicio
+					if (autonomiaSeleccionada.equals( (String)value )) {
+						c.setBackground( Color.CYAN );
+					}
+				}
+				return c;
+			}
+		} );
+		
+		// Paso 8
+		tablaDatos.addMouseMotionListener( new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				int filaEnTabla = tablaDatos.rowAtPoint( e.getPoint() );
+				int colEnTabla = tablaDatos.columnAtPoint( e.getPoint() );
+				if (colEnTabla == 2) {
+					int numHabs = datosMunis.getListaMunicipios().get(filaEnTabla).getHabitantes();
+					tablaDatos.setToolTipText( String.format( "Población: %,d", numHabs ) );
+				} else {
+					tablaDatos.setToolTipText( null );  // Desactiva
+				}
+			}
+		});
+
+		// Paso 11
+		tablaDatos.addMouseListener( new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int filaEnTabla = tablaDatos.rowAtPoint( e.getPoint() );
+				int colEnTabla = tablaDatos.columnAtPoint( e.getPoint() );
+				if (colEnTabla == 4 && filaEnTabla>=0) {
+					autonomiaSeleccionada = datosMunis.getListaMunicipios().get(filaEnTabla).getAutonomia();
+				} else {
+					autonomiaSeleccionada = "";
+				}
+				tablaDatos.repaint();
+			}
+		});
+
+		// Paso 12
+		tablaDatos.setDefaultEditor( Integer.class, new DefaultCellEditor( new JTextField() ) {
+			SpinnerNumberModel mSpinner = new SpinnerNumberModel( 200000, 200000, 5000000, 1000 );
+			JSpinner spinner = new JSpinner( mSpinner );
+			boolean lanzadoSpinner;
+			@Override
+			public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) { // Componente que se pone en la tabla al editar una celda
+				if (column != 2) {
+					lanzadoSpinner = false;
+					return super.getTableCellEditorComponent(table, value, isSelected, row, column);
+				}
+				mSpinner.setValue( (Integer) value );
+				lanzadoSpinner = true;
+				return spinner;
+			}
+			@Override
+			public Object getCellEditorValue() { // Valor que se retorna al acabar la edición
+				if (lanzadoSpinner) {
+					return spinner.getValue();
+				} else {
+					return Integer.parseInt( super.getCellEditorValue().toString() );
+				}
+			}
 		});
 		
 	}
 	
 	private class MiTableModel implements TableModel {
 
+		// Paso 7
+		private final Class<?>[] CLASES_COLS = { Integer.class, String.class, Integer.class, String.class, String.class };
 		@Override
 		public Class<?> getColumnClass(int columnIndex) {
-			// System.out.println( "getColumnClass" + columnIndex );
-			return Object.class;
+			// Si no se quiere especializar cada columna:
+			// return Object.class;
+			return CLASES_COLS[columnIndex]; // Paso 7
 		}
 
 		@Override
@@ -158,7 +259,10 @@ public class VentanaTablaDatos extends JFrame {
 				break;
 			case 2:
 				try {
-					datosMunis.getListaMunicipios().get(rowIndex).setHabitantes( Integer.parseInt((String)aValue) );
+					// Cuando no estaba especializada la columna había que tratarla como Object
+					// datosMunis.getListaMunicipios().get(rowIndex).setHabitantes( Integer.parseInt((String)aValue) );
+					// Pero ahora puede tratarse como Integer:
+					datosMunis.getListaMunicipios().get(rowIndex).setHabitantes( (Integer) aValue );
 				} catch (NumberFormatException e) {
 					JOptionPane.showMessageDialog( VentanaTablaDatos.this, "Nº de habitantes erróneo" );
 				}
@@ -194,9 +298,15 @@ public class VentanaTablaDatos extends JFrame {
 			}
 		}
 		
+	    // Paso 5
 		public void borraFila( int fila ) {
 			fireTableChanged( new TableModelEvent( modeloDatos, fila, datosMunis.getListaMunicipios().size() ));
 		}
 		
+	    // Paso 6
+	    public void anyadeFila( int fila ) {
+	    	fireTableChanged( new TableModelEvent( modeloDatos, fila, datosMunis.getListaMunicipios().size() ) );  // Para que detecte el cambio en todas
+	    }
+	    
 	}
 }
